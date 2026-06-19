efdb2ecc41cc76d715e3d24dc79ddb45e660666d
Robin Candau <robincandau@protonmail.com>
2026-06-19 22:54:13 +0200

    Switch to clap for arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
 //! Manora - A simple CLI / TUI tool to display (or save) man pages as PDFs.
 
 // Import external modules
+use clap::{ArgAction, Parser};
 use std::{env, process};
 
 // Import internal modules
 mod help;
 mod tmpdir;
 
+// Parse arguments
+#[derive(Parser)]
+#[command(
+    disable_help_flag = true,
+    disable_version_flag = true
+)]
+struct Args {
+    #[arg(short = 'h', long = "help", action = ArgAction::SetTrue)]
+    help: bool,
+
+    #[arg(short = 'V', long = "version", action = ArgAction::SetTrue)]
+    version: bool,
+
+    option: Option<String>,
+    man_page: Option<String>,
+    file: Option<String>,
+}
+
 fn main() {
     // Parse arguments
-    let args: Vec<String> = env::args().collect();
-    let has_arg = |flag: &str| args.iter().any(|arg| arg == flag);
+    let args = Args::parse();
 
     // Show help message if the -h / --help arg is passed
-    if has_arg("-h") || has_arg("--help") {
+    if args.help {
         help::show_help();
         return;
     }
 
     // Show name and version if the -V / --version arg is passed
-    if has_arg("-V") || has_arg("--version") {
+    if args.version {
         println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
         return;
     }
