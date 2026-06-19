45dee408da6cbd6e5d7bce658cad39d9c71b1981
Robin Candau <robincandau@protonmail.com>
2026-06-19 17:29:55 +0200

    Drop log crate dependencies
    I don't think we'll need this for such a simple project

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 //! Manora - A simple CLI / TUI tool to display (or save) man pages as PDFs.
 
 // Import external modules
-//use log::{error, info};
 use std::env;
 
 // Import internal modules
 mod help;
 
 fn main() {
-    // Initialize logger
-    env_logger::init();
-
     // Parse arguments
     let args: Vec<String> = env::args().collect();
     let has_arg = |flag: &str| args.iter().any(|arg| arg == flag);
