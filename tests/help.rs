2d6762b06ad1d4ab675f08e93bcdb37dd253b483
Robin Candau <robincandau@protonmail.com>
2026-06-21 00:48:59 +0200

    Add a test for the help arg

--- tests/help.rs
+++ tests/help.rs
@@ -1,14 +1,13 @@
-//! Test the --version argument
+//! Test the --help argument
 
 use assert_cmd::Command;
 use predicates::str::contains;
 
 #[test]
-fn version_arg() {
+fn help_arg() {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
 
-    cmd.arg("--version")
+    cmd.arg("--help")
         .assert()
-        .success()
-        .stdout(contains(env!("CARGO_PKG_VERSION")));
+        .success();
 }
